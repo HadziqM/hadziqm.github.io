402f87bf4734ff2eb8ff840ffd47be5a7b8539f4
HadziqM <50319538+HadziqM@users.noreply.github.com>
2022-11-18 11:28:34 +0700

    setting up docs

--- docs/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-3ea730d1df777f88.js","static/chunks/pages/index-20355374c3dbc8ea.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
